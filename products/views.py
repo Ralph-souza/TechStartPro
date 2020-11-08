839bd8618e29f670e238477757e67e4c9540b686
ralph <ralph.souza@itmss.com.br>
2020-11-08 15:48:37 -0300

    Desenvolvendo a segunda etapa do CRUD com CREATE.

--- products/views.py
+++ products/views.py
@@ -9,15 +9,15 @@ def list_products(request):
     products = Product.objects.all()
     return render(request, 'products.html', {'products': products})
 
-# #Creating process
-# def create_product(request):
-#     form = ProductForm(request.POST or None)
+#Creating process
+def create_product(request):
+    form = ProductForm(request.POST or None)
 
-#     if form.is_valid():
-#         form.save()
-#         return redirect('list_products')
+    if form.is_valid():
+        form.save()
+        return redirect('list_products')
 
-#     return render(request, 'products-form.html', {'form': form})
+    return render(request, 'products-form.html', {'form': form})
 
 # #Updating process
 # def update_product(request, id):
