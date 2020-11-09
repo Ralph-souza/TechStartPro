002ed840098d70ad395c4999d0bcec41c218d912
ralph <ralph.souza@itmss.com.br>
2020-11-09 19:43:26 -0300

    Desenvolvendo e reformulando o código: Nesta etapa criei os dois últimos processos do CRUD "update & delete", e fiz o vinculo via urls, contudo os mesmos ainda apresentam falhas as quais estou buscando corrigir.

--- products/views.py
+++ products/views.py
@@ -15,6 +15,27 @@ def create_products(request):
 
     if form.is_valid():
         form.save()
-        return redirect('list_products')
+        return redirect('products:list_products')
 
     return render(request, 'products/products-form.html', {'form': form}) 
+
+# Updating process of the CRUD
+def update_product(request, id):
+    product = Product.objects.get(id=id)
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid():
+        form.save()
+        return redirect('products:list_products')
+
+    return render(request, 'products/products-form.html', {"form": form, 'product': product})
+
+# Deleting process of the CRUD
+def delete_product(request, id):
+    product = Product.objects.get(id=id)
+
+    if request.method == 'POST':
+        product.delete()
+        return redirect('products:list_products')
+
+    return render(request, 'products/prod-delete-confirm.html', {'product': product})
