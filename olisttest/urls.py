f97a8b8a9c8f3b2699b5182fac7003ea77dcda6e
ralph <ralph.souza@itmss.com.br>
2020-11-09 16:29:59 -0300

    Desenvolvendo e reformulando código: Nesta etapa estou recriando os processos de CRUD a iniciar pelas configurações de parâmetros do app "products".

--- olisttest/urls.py
+++ olisttest/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', include('products.urls', namespace='products')),
     path('', include('csvs.urls', namespace='csvs')),
 ]
 
