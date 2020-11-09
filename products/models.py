83ea1c72483bcd7246ee7621b9452b4f32929826
ralph <ralph.souza@itmss.com.br>
2020-11-09 16:56:35 -0300

    Desenvolvendo e reformulando o código: Nesta etapa forma refeitas as estruturas de models em products assim as views e a resstruturação dos templates na mesma.

--- products/models.py
+++ products/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+# Products Data Base
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    value = models.DecimalField(max_digits=9, decimal_places=2)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+
+    def __str__(self):
+        return self.category
+
+# Categories Data Base
 CATEGORY_CHOICES = (
     ('MOVEIS', 'moveis'),
     ('DECORACAO', 'decoracao'),
--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+# Reading process od the CRUD
+def list_products(request):
+    products = Product.objects.all()
+    return render(request, 'products.html', {'products': products})
